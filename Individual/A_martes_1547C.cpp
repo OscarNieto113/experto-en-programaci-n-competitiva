5640e0c8b646b7447a5237c9ef82606cdac67b95
OscarNieto113 <a01705090@tec.mx>
2023-03-21 14:32:05 -0600

    Terminado

--- Individual/A_martes_1547C.cpp
+++ Individual/A_martes_1547C.cpp
@@ -1,40 +1,60 @@
 #include <iostream>
 #include <vector>
-#include <bits/stdc++.h>
-
 using namespace std;
 
-void solution (<int>k, <int>MC, <int>PC, vector<int>ML, vector<int>CL){
-    for (int i = 0; i < MC; i++){
-        if (ML[i] == 0) k++;
-    }
-
-    for (int i = 0; i < PC; i++){
-        if (PL[i] == 0) k++;
-    }
-
+void solution (){
+    int k, m, p;
+    cin >> k >> m >> p;
+    vector<int> ml(m), pl(p);
+    for (int i = 0; i < m; i++) cin >> ml[i];
+    for (int i = 0; i < p; i++) cin >> pl[i];
     
+    int i=0, j=0;
+    vector<int>visited;
+
+    while (i < m || j < p) {
+        if (i < m && j < p) {
+            if (ml[i] == 0) visited.push_back(0), i++, k++;
+            else if (pl[j] == 0) visited.push_back(0), j++, k++;
+            else {
+                if (ml[i] < pl[j] && ml[i] <= k) visited.push_back(ml[i]), i++;
+                else if (ml[i] >= pl[j] && pl[j] <= k) visited.push_back(pl[j]), j++;
+                else{
+                    cout << -1 << "\n";
+                    return;
+                }
+            }
+        }
+
+        else if(i < m){
+            if (ml[i] == 0) visited.push_back(0), i++, k++;
+            else {
+                if (ml[i] <= k) visited.push_back(ml[i]), i++;
+                else {
+                    cout << -1 << "\n";
+                    return;
+                }
+            }
+        }
+        else if(j < p){
+            if (pl[j] == 0) visited.push_back(0), j++, k++;
+            else {
+                if (pl[j] <= k) visited.push_back(pl[j]), j++;
+                else {
+                    cout << -1 << "\n" ;
+                    return;
+                }
+            }
+        }
+    }
+    for(auto i : visited) cout << i << " " ;
+        cout << "\n";
 }
 
 int main(int argc, char* argv[]) {
-  int testCases, initialLines, MonocarpChanges, PolycarpChanges, aux;
-  vector<int> MonocarpLog, PolycarpLog;
-
-  cin >> testCases;
-  for (int i = 0; i < testCases; i++){
-    cin >> initialLines >> MonocarpChanges >> PolycarpChanges;
-
-    for (int i = 0; i < MonocarpChanges; i++){
-        cin >> aux;
-        MonocarpLog.push_back(aux);
+    int testCases;
+    cin >> testCases;
+    for (int i = 0; i < testCases; i++){
+        solution();
     }
-
-    for (int i = 0; i < PolycarpChanges; i++){
-        cin >> aux;
-        PolycarpLog.push_back(aux);
-    }
-
-
-
-  }
 }
