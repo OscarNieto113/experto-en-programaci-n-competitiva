079ab0dc1e1ba6c7ee165b4100c9fddb902abcab
OscarNieto113 <a01705090@tec.mx>
2023-03-21 23:52:16 -0600

    Finished

--- Team/B_martes.cpp
+++ Team/B_martes.cpp
@@ -1,46 +1,40 @@
 #include <iostream>
-#include <vector>
 #include <string>
 #include <bits/stdc++.h>
-
-
+ 
 using namespace std;
-
-
+ 
 void solution (){
-    int sizeRestaurant, distance, gap, j;
-    int tablesTaken = 0;
     string arrange;
-
-    cin >> sizeRestaurant, distance;
-    cout << sizeRestaurant << distance << endl;
+    int sizeRestaurant, distance, gap, gap2;
+    int tablesTaken = 0;
+    
+    cin >> sizeRestaurant >> distance;
     cin >> arrange;
-    cout << arrange;
-
+ 
     gap = distance;
-    j = -(distance + 1);
-
+    gap2 = -(distance + 1);
+ 
     for (int i = 0; i < sizeRestaurant; i++){
         if (arrange[i] == '1') {
             gap = 0;
-            if ((i + 1 - j) <= distance)
-				--tablesTaken;
-			j = i + 1;
+            if ((i + 1 - gap2) <= distance) tablesTaken--;
+
+			gap2 = i + 1;
         } else {
             if (++gap > distance) {
-				++tablesTaken;
-				j = i + 1;
-				gap = 0;
+				tablesTaken++;
+				gap2 = i + 1, gap = 0;
 			}
         }
     }
-
-    //cout << tablesTaken << endl;
+ 
+    cout << tablesTaken << endl;
 }
-
+ 
 int main(int argc, char* argv[]) {
     int testCases;
     cin >> testCases;
-
+ 
     for (int i = 0; i < testCases; i++) solution();
 }
