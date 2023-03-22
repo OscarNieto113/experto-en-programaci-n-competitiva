69ebc4d5908cb309200d8c1d626e9afdefd61321
OscarNieto113 <a01705090@tec.mx>
2023-03-22 11:01:27 -0600

    Completed

--- Individual/E_miercoles_1511B.cpp
+++ Individual/E_miercoles_1511B.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 void solution (){
-    int x, y, z;
+    long long x, y, z;
     cin >> x >> y >> z;
+
+    long long A = pow(10, z-1);
+    long long B = pow(10, z-1);
+
+    while (A < (long long)pow(10, x-1)) A = A * 2;
+
+    while (B < (long long)pow(10, y-1)) B = B * 3;
+
+    cout << A << " " << B << endl;
 
 }
 
