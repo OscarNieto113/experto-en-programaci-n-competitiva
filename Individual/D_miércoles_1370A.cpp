f67664ed5fec73fde7180e14d3ea9dacb49032e4
OscarNieto113 <a01705090@tec.mx>
2023-03-22 09:37:09 -0600

    a

---
+++ Individual/D_miércoles_1370A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+void solution (){
+
+}
+
+int main(int argc, char* argv[]) {
+    int testCases;
+
+    cin >> testCases;
+
+    for (int i = 0; i < width - 1; i++) solution();
+}
