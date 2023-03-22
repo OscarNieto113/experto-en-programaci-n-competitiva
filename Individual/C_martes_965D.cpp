c21791f27af57ca3953df8ab328831d13ef5d740
OscarNieto113 <a01705090@tec.mx>
2023-03-21 19:38:23 -0600

    Finished

--- Individual/C_martes_965D.cpp
+++ Individual/C_martes_965D.cpp
@@ -3,17 +3,32 @@
 #include <vector>
 using namespace std;
 
-void solution (){
+void solution (int width, int maxJump, vector<int>rocks){
+    int sum = 0;
+    for (int i = 0; i < maxJump; i++){
+        sum += rocks[i];
+    }
 
+    int maxFrogs = sum;
+
+    for (int i = maxJump; i < width - 1; i++) {
+        sum = sum - rocks[i - maxJump] + rocks[i];
+        maxFrogs = min(maxFrogs, sum);
+    }
+
+    cout << maxFrogs;
 }
 
 int main(int argc, char* argv[]) {
     int width, maxJump;
-    
+
     cin >> width >> maxJump;
+
     vector <int> rocks(width - 1);
+
     for (int i = 0; i < width - 1; i++){
         cin >> rocks[i];
     }
-    solution();
+
+    solution(width, maxJump, rocks);
 }
