9e0b52bac556f6797b2fc58c2f9c8148d97c1cab
OscarNieto113 <a01705090@tec.mx>
2023-03-21 23:24:53 -0600

    a

--- Team/B_martes.cpp
+++ Team/B_martes.cpp
@@ -8,7 +8,34 @@ using namespace std;
 
 
 void solution (){
+    int sizeRestaurant, distance, gap, j;
+    int tablesTaken = 0;
+    string arrange;
 
+    cin >> sizeRestaurant, distance;
+    cout << sizeRestaurant << distance << endl;
+    cin >> arrange;
+    cout << arrange;
+
+    gap = distance;
+    j = -(distance + 1);
+
+    for (int i = 0; i < sizeRestaurant; i++){
+        if (arrange[i] == '1') {
+            gap = 0;
+            if ((i + 1 - j) <= distance)
+				--tablesTaken;
+			j = i + 1;
+        } else {
+            if (++gap > distance) {
+				++tablesTaken;
+				j = i + 1;
+				gap = 0;
+			}
+        }
+    }
+
+    //cout << tablesTaken << endl;
 }
 
 int main(int argc, char* argv[]) {
